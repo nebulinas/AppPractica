c933e190c64fc89644abc854a6063675a6a7ad15
Herielis <tajiboyherielis@gmail.com>
2025-09-16 08:12:24 -0600

    se agrego la configuracion de moviemnto entre activitys y se modifico el id main del xml

--- app/src/main/java/com/example/apppractica/MainActivity.java
+++ app/src/main/java/com/example/apppractica/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.apppractica;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton btnComputadora;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnComputadora = findViewById(R.id.btn_menu);
+        btnComputadora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, MenuComputadoraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
-
 }
